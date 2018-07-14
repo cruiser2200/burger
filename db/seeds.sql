1e570ba45167057731d86287db3d6d39e86ac413
Mike Smith <mike.smith1@earthlink.net>
2018-07-13 22:51:34 -0500

    trying again to commit

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Plain Burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie Burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Burger Deluxe", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Cheese Burger", 0);
